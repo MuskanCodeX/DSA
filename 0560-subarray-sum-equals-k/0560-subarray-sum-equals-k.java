f1db8f528a4358c43c5037ddc22ff84546153611
MuskanCodeX <muskansuhag1205@gmail.com>
2026-08-08 23:23:42 +0530

    Time: 1548 ms (12.57%), Space: 46.3 MB (99.97%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n=nums.length;
+        //------BRUTE FORCE------
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         int sum=0;
+        //         for(int m=i;m<=j;m++){
+        //             sum += nums[m];
+        //         }
+        //         if(sum==k){
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        //----------BETTER---------
+        int count=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum += nums[j];
+                if(sum==k){
+                count++;
+                }
+            }
+        }
+        return count;
+    }
+}
