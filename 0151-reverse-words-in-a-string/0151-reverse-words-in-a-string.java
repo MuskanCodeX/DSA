63151e8c011172948fa25d428df511a831b9ea99
MuskanCodeX <muskansuhag1205@gmail.com>
2026-08-19 17:59:04 +0530

    Time: 2 ms (99.89%), Space: 43.6 MB (98.4%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,15 +1,47 @@
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim();
-        String[] words = s.split("\\s+");
-        StringBuilder ans =  new StringBuilder();
-        for(int i=words.length-1;i>=0;i--){
-            if(i==0){
-                ans.append(words[i]);
-            }else{
-            ans.append(words[i]).append(" ");
+        // s = s.trim();
+        // String[] words = s.split("\\s+");
+        // StringBuilder ans =  new StringBuilder();
+        // for(int i=words.length-1;i>=0;i--){
+        //     if(i==0){
+        //         ans.append(words[i]);
+        //     }else{
+        //     ans.append(words[i]).append(" ");
+        //     }
+        // }
+        // return ans.toString();
+
+        char[] arr = s.toCharArray();
+        int n= arr.length;
+        int idx = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i] != ' '){
+                if(idx != 0){
+                    arr[idx++] = ' ';
+                }
+                while(i<n && arr[i] != ' '){
+                    arr[idx++] = arr[i++];
+                }
             }
         }
-        return ans.toString();
+        reverse(arr, 0, idx-1);
+        int start = 0;
+        for(int i=0;i<=idx;i++){
+            if(i == idx || arr[i] == ' '){
+                reverse(arr, start , i-1);
+                start = i+1;
+            }
+        }
+        return new String(arr, 0, idx);
+    }
+    private void reverse(char[] arr, int start , int end){
+        while(start<end){
+            char temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
     }
 }
