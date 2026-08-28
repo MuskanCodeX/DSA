e0398756fea134a52e60af2d499fca5b896cbaaf
MuskanCodeX <muskansuhag1205@gmail.com>
2026-08-28 14:58:19 +0530

    Time: 1 ms (99.89%), Space: 55.3 MB (69.05%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        int el=0;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                el=nums[i];
+                count=1;
+            }else if(el==nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return el;
+    }
+}
