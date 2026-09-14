7cbf16d67593961ab7884f651028138bbff4b43b
MuskanCodeX <muskansuhag1205@gmail.com>
2026-09-14 17:57:01 +0530

    Time: 1 ms (99.28%), Space: 47.2 MB (89.2%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,22 +1,47 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int ans[] = new int[2];
-        HashMap<Integer,Integer> map = new HashMap<>();
+        // int ans[] = new int[2];
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // int n = grid.length;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         int num = grid[i][j];
+        //         map.put(num, map.getOrDefault(num, 0)+1);
+        //     }
+        // }
+        // for(int i=1;i<=n*n;i++){
+        //     if(map.getOrDefault(i, 0) == 2){
+        //         ans[0] = i;
+        //     }
+        //     if(map.getOrDefault(i, 0) == 0){
+        //         ans[1] = i;
+        //     }
+        // }
+        // return ans;
+
+        //-----------optimal--------------
         int n = grid.length;
+        int total = n*n;
+        int expSum = 0;
+        int actSum = 0;
+        int sqExpSum = 0;
+        int sqActSum = 0;
+        for(int i=1;i<=total;i++){
+            expSum += i;
+            sqExpSum += (long)i*i;
+        }
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 int num = grid[i][j];
-                map.put(num, map.getOrDefault(num, 0)+1);
+                actSum += num;
+                sqActSum += (long)num*num;
             }
         }
-        for(int i=1;i<=n*n;i++){
-            if(map.getOrDefault(i, 0) == 2){
-                ans[0] = i;
-            }
-            if(map.getOrDefault(i, 0) == 0){
-                ans[1] = i;
-            }
-        }
-        return ans;
+        long diff = actSum - expSum;
+        long sqdiff = sqActSum - sqExpSum;
+        long sum = sqdiff / diff;
+        int repeated = (int)(diff + sum)/2;
+        int missing = (int)(sum - repeated);
+        return new int[]{repeated, missing};
     }
 }
