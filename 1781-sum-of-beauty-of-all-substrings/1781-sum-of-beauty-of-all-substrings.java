7ce137ee63414cff98cea903ac3ff430097a2844
MuskanCodeX <muskansuhag1205@gmail.com>
2026-09-15 17:48:09 +0530

    Time: 60 ms (78.58%), Space: 44.3 MB (47.07%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -2,17 +2,35 @@ class Solution {
     public int beautySum(String s) {
         int n = s.length();
         int sum = 0;
+        //-------using hashmap-----------
+        // for(int i=0;i<n;i++){
+        //     HashMap<Character,Integer> map = new HashMap<>();
+        //     for(int j=i;j<n;j++){
+        //         map.put(s.charAt(j), map.getOrDefault(s.charAt(j),0) +1);
+        //         int maxi =  Integer.MIN_VALUE;
+        //         int mini = Integer.MAX_VALUE;
+        //         for(int val : map.values()){
+        //             mini = Math.min(mini, val);
+        //             maxi = Math.max(maxi, val);
+        //         }
+        //         sum += (maxi-mini);
+        //     }
+        // }
+
+        //-------using array----------
         for(int i=0;i<n;i++){
-            HashMap<Character,Integer> map = new HashMap<>();
+            int freq[] = new int[26];
             for(int j=i;j<n;j++){
-                map.put(s.charAt(j), map.getOrDefault(s.charAt(j),0) +1);
-                int maxi =  Integer.MIN_VALUE;
+                freq[s.charAt(j) - 'a']++;
+                int maxi = 0;
                 int mini = Integer.MAX_VALUE;
-                for(int val : map.values()){
-                    mini = Math.min(mini, val);
-                    maxi = Math.max(maxi, val);
+                for(int k=0;k<26;k++){
+                    if(freq[k] > 0){
+                        maxi = Math.max(maxi, freq[k]);
+                        mini = Math.min(mini, freq[k]);
+                    }
                 }
-                sum += (maxi-mini);
+                sum += maxi - mini;
             }
         }
         return sum;
